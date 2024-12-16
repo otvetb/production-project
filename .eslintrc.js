0e531a2bfbac36f4bbb67febfd5875549198169c
Maxim <matsiplyuk@yandex.ru>
2024-12-16 06:34:09 +0400

    add translstion to navbar

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': [
-            'error', { markupOnly: true, ignoreAttribute: ['data-testid'] },
+            'error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
