9e0ea7e0465bbaeb7183fe79368f078601cbc9ef
Maxim <matsiplyuk@yandex.ru>
2024-12-21 10:47:11 +0400

    12

--- src/shared/ui/PageLoader/PageLoader.tsx
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { FC } from 'react';
-import L123 from 'shared/ui/Loader/L123';
-import Loader from '../Loader/Loader';
+
 import cls from './PageLoader.module.scss';
 
 interface PageLoaderProps {
