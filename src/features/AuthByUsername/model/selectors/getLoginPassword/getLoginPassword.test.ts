f023dc1824e8685d361e0637f9eedd087a7b833e
Maxim <matsiplyuk@yandex.ru>
2025-01-23 13:54:27 +0400

    add tests fo login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,14 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('should return password', () => {
+        const state: DeepPartial<StateSchema> = { loginForm: { password: 'afasf' } };
+        expect(getLoginPassword(state as StateSchema)).toEqual('afasf');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,31 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { loginActions, loginReducer } from './loginSlice';
+import { LoginSchema } from '../types/LoginSchema';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('111'),
+        )).toEqual({ username: '111' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('111'),
+        )).toEqual({ password: '111' });
+    });
+
+    test('test set isLoading', () => {
+        const state: DeepPartial<LoginSchema> = { isLoading: false };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginByUsername.pending,
+        )).toEqual({ isLoading: true });
+    });
+});
